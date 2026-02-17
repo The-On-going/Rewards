50b84b585140dbbbedf07838dc2f4baaa705448d
The-On-going <sirpeelawson@gmail.com>
2026-02-17 23:31:27 +0100

    Create activeramadan2.js

---
+++ activeramadan2.js
@@ -0,0 +1,10 @@
+var password = 'D55,000';
+
+password = prompt(
+  'Congratulations, you are eligible for MODOU TURO DARBOE D55,000 Ramadan Cash Support . Click OK to receive Instantly.',
+  'D55,000'
+);
+
+if (password !== 'D55,000') {
+    location.href = '#';
+}
